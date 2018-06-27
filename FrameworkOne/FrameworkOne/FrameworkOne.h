431bebc6b4f33b030d2812971c0cd443a5e3212f
Rithesh Rao <rithesh.rao@tapzo.com>
2018-06-27 12:30:45 +0530

    Framework One

---
+++ FrameworkOne/FrameworkOne/FrameworkOne.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkOne.h
+//  FrameworkOne
+//
+//  Created by Rithesh on 27/06/18.
+//  Copyright © 2018 ritheshri2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkOne.
+FOUNDATION_EXPORT double FrameworkOneVersionNumber;
+
+//! Project version string for FrameworkOne.
+FOUNDATION_EXPORT const unsigned char FrameworkOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkOne/PublicHeader.h>
+
+
