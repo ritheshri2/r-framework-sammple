4cc522cca598e46b7a4c8ba040da8dafab83b798
Rithesh Rao <rithesh.rao@tapzo.com>
2018-06-27 12:43:00 +0530

    Framework Sample

---
+++ FrameworkCore/FrameworkCore/FrameworkCore.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkCore.h
+//  FrameworkCore
+//
+//  Created by Rithesh on 27/06/18.
+//  Copyright © 2018 ritheshri2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkCore.
+FOUNDATION_EXPORT double FrameworkCoreVersionNumber;
+
+//! Project version string for FrameworkCore.
+FOUNDATION_EXPORT const unsigned char FrameworkCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkCore/PublicHeader.h>
+
+
